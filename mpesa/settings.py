1431fe070477f612448df5a89ef40cec7b0d7fc5
collins <collinsmathayo1@gmail.com>
2026-09-03 11:45:01 +0300

    changes

--- mpesa/settings.py
+++ mpesa/settings.py
@@ -30,9 +30,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#$8^%tc)xsw)6e79fp-%292_i&bdf3770fd=4ck64zdi*gbn@d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    
+]
 
 
 # Application definition
